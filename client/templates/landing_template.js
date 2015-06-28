da822dbba4ca7ff4d1b7d670e2bc64bd0fc2891e
siddygups <siddharthgupta@Siddharths-MBP.home>
2015-06-28 15:46:40 -0400

    first iteration - looks good but needs templates fixed - also font problem - next is waiting list

---
+++ client/templates/landing_template.js
@@ -0,0 +1,18 @@
+ Template.landingTemplate.rendered = function(){
+	 $(function(){
+	      $(".typed").typed({
+	        strings: ["Check Twitter . . .","Watch Game of Thrones . . .", "Read Buzzfeed . . .", "Buy skinny jeans . . .", "Peruse reddit . . .", "Check Twitter / Watch Game of Thrones / Read Buzzfeed / Buy skinny jeans / Peruse reddit . . ."],
+			  typeSpeed: 30,
+			  backSpeed: 20,
+              loop: false,
+			  backDelay: 500
+	      });
+	  });
+	  
+	  var options = [
+		  {selector: '.section1', offset: 400, callback: 'Materialize.showStaggeredList(".section1")' }
+	  ];
+	  Materialize.scrollFire(options);
+      
+
+ }

7a59d32814285e22c72beb351106b887265ccc25
siddygups <siddharthgupta@Siddharths-MacBook-Pro.local>
2015-06-28 18:52:28 -0400

    really sloppy firescroll animation added - review ul / li, js implementation, condense css

--- client/templates/landing_template.js
+++ client/templates/landing_template.js
@@ -1,4 +1,5 @@
  Template.landingTemplate.rendered = function(){
+	 Materialize.showStaggeredList(".section2")
 	 $(function(){
 	      $(".typed").typed({
 	        strings: ["Check Twitter . . .","Watch Game of Thrones . . .", "Read Buzzfeed . . .", "Buy skinny jeans . . .", "Peruse reddit . . .", "Check Twitter / Watch Game of Thrones / Read Buzzfeed / Buy skinny jeans / Peruse reddit . . ."],
@@ -12,6 +13,7 @@
 	  var options = [
 		  {selector: '.section1', offset: 400, callback: 'Materialize.showStaggeredList(".section1")' }
 	  ];
+	  
 	  Materialize.scrollFire(options);
       
 

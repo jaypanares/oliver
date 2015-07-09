fa854f1ed63716f2308dbecef07011077be80ba1
jaypanares <jaypanares@gmail.com>
2015-07-09 20:08:32 +0800

    Added server-side validation for insert

--- methods.js
+++ methods.js
@@ -1,43 +1,33 @@
-/*globals Emails*/
-
 Meteor.methods({
   addEmail: function (fieldVal, ip, refId) {
-
-    var duplicateEmail = Emails.findOne({
-      email: fieldVal
-    });
-
-    var ipReuse = Emails.find({
-      ipAddress: ip
-    }).count();
+    var duplicateEmail = Emails.findOne({email: fieldVal});
+    var ipReuse = Emails.find({ipAddress: ip}).count();
 
     if (!fieldVal) {
       return -1;
     }
-
     if (duplicateEmail) {
       return duplicateEmail._id;
     }
-
     if (ipReuse) {
       return 0;
     }
 
-    var returnId = Emails.insert({
+    insertObj = {
       email: fieldVal,
       createdAt: new Date(),
       ipAddress: ip,
       referrer: refId,
       referrals: []
-    });
+    };
 
-    Emails.update({
-      _id: refId
-    }, {
-        $push: {
-          referrals: returnId
-        }
-      });
+    check(insertObj, Schemas.Emails);
+
+    var returnId = Emails.insert(insertObj);
+
+    Emails.update({_id: refId}, {
+      $push: {referrals: returnId}
+    });
 
     return returnId;
   }
